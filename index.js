82874bd5c9a36615225d8cae71d5a8bbea7366bc
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-01-07 20:54:00 +0530

    Add filters on UIusing javascript

---
+++ index.js
@@ -0,0 +1,93 @@
+const filters= {
+    Brightness:{
+        value:100,
+        min:0,
+        max:200,
+        unit:'%'
+    },
+    Contrast:{
+        value:100,
+        min:0,
+        max:200,
+        unit:'%'
+    },
+    Exposure:{
+        value:100,
+        min:0,
+        max:200,
+        unit:'%'
+    },
+    Saturation:{
+        value:100,
+        min:0,
+        max:200,
+        unit:'%'
+    },
+    HueRotation:{
+        value:0,
+        min:0,
+        max:360,
+        unit:'deg'
+    },
+    Blur:{
+        value:0,
+        min:0,
+        max:20,
+        unit:'px'
+    },
+    GrayScale:{
+        value:0,
+        min:0,
+        max:100,
+        unit:'%'
+    },
+    Sepia:{
+        value:0,
+        min:0,
+        max:100,
+        unit:'%'
+    },
+    Opacity:{
+        value:100,
+        min:0,
+        max:100,
+        unit:'%'
+    },
+    Invert:{
+        value:0,
+        min:0,
+        max:100,
+        unit:'%'
+    },
+}
+
+const createFilterElements= (name, unit='%', value, min, max) =>{
+
+    const div= document.createElement('div');
+    div.classList.add('filter');
+
+    const input= document.createElement('input');
+    input.type= 'range';
+    input.min= min;
+    input.max= max;
+    input.value= value;
+    input.name= name;
+
+    const p= document.createElement('p');
+    p.innerText= name;
+
+    div.appendChild(p);
+    div.appendChild(input);
+
+    return div;
+
+}
+
+const filtersContainer= document.querySelector('.filters');
+Object.keys(filters).forEach(filter=>{
+    const filterElement= createFilterElements(filter, filters[filter].unit, filters[filter].value, filters[filter].min, filters[filter].max);
+
+    filtersContainer.appendChild(filterElement);
+    
+
+})
