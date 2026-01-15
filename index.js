2751d2faf3cadcb5ef394f1bc3d741ca8aeed275
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-01-15 23:22:45 +0530

    Added filters on UI and functionalities and completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const filters= {
+let filters= {
     Brightness:{
         value:100,
         min:0,
@@ -6,12 +6,6 @@ const filters= {
         unit:'%'
     },
     Contrast:{
-        value:100,
-        min:0,
-        max:200,
-        unit:'%'
-    },
-    Exposure:{
         value:100,
         min:0,
         max:200,
@@ -64,8 +58,11 @@ const filters= {
 const canvasImg= document.querySelector('#image-canvas');
 const inputImg= document.querySelector('#image-input');
 const canvasCtx= canvasImg.getContext('2d'); 
+const resetBtn= document.querySelector('#reset-btn');
 let file= null;
 let image= null;
+let filtersContainer= document.querySelector('.filters');
+const downloadBtn= document.querySelector('#download-btn');
 
 const createFilterElements= (name, unit='%', value, min, max) =>{
 
@@ -87,26 +84,32 @@ const createFilterElements= (name, unit='%', value, min, max) =>{
 
     input.addEventListener('input', (event)=>{
         filters[name].value= input.value;
-        console.log(name, filters[name])
+        // console.log(name, filters[name])
+        applyFilters()
     })
 
     return div;
 
 }
 
-const filtersContainer= document.querySelector('.filters');
-Object.keys(filters).forEach(filter=>{
-    const filterElement= createFilterElements(filter, filters[filter].unit, filters[filter].value, filters[filter].min, filters[filter].max);
 
-    filtersContainer.appendChild(filterElement);
+
+function createFilter(){
+    
+    Object.keys(filters).forEach(filter=>{
+        const filterElement= createFilterElements(filter, filters[filter].unit, filters[filter].value, filters[filter].min, filters[filter].max);
+
+        filtersContainer.appendChild(filterElement);
     
 
-})
+    })
+}
 
-
+createFilter();
 
 inputImg.addEventListener('change',()=>{
     const imagePlaceHolder= document.querySelector('.placeholder');
+    canvasImg.style.display= 'block';
     imagePlaceHolder.style.display= 'none';
     file= event.target.files[0];
     const img= new Image(); //!creating image by using JS instead html
@@ -117,4 +120,92 @@ inputImg.addEventListener('change',()=>{
         canvasImg.height= img.height;
         canvasCtx.drawImage(img, 0, 0)
     }
+})
+
+
+function applyFilters(){
+    canvasCtx.clearRect(0, 0, canvasImg.width, canvasImg.height);
+    canvasCtx.filter= `
+    brightness(${filters.Brightness.value}${filters.Brightness.unit})
+    contrast(${filters.Contrast.value}${filters.Contrast.unit})
+    saturate(${filters.Saturation.value}${filters.Saturation.unit})
+    hue-rotate(${filters.HueRotation.value}${filters.HueRotation.unit})
+    blur(${filters.Blur.value}${filters.Blur.unit})
+    grayscale(${filters.GrayScale.value}${filters.GrayScale.unit})
+    sepia(${filters.Sepia.value}${filters.Sepia.unit})
+    opacity(${filters.Opacity.value}${filters.Opacity.unit})
+    invert(${filters.Invert.value}${filters.Invert.unit})
+    `
+    canvasCtx.drawImage(image, 0, 0)
+}
+
+
+resetBtn.addEventListener('click', ()=>{
+    filters= {
+    Brightness:{
+        value:100,
+        min:0,
+        max:200,
+        unit:'%'
+    },
+    Contrast:{
+        value:100,
+        min:0,
+        max:200,
+        unit:'%'
+    },
+    Saturation:{
+        value:100,
+        min:0,
+        max:200,
+        unit:'%'
+    },
+    HueRotation:{
+        value:0,
+        min:0,
+        max:360,
+        unit:'deg'
+    },
+    Blur:{
+        value:0,
+        min:0,
+        max:20,
+        unit:'px'
+    },
+    GrayScale:{
+        value:0,
+        min:0,
+        max:100,
+        unit:'%'
+    },
+    Sepia:{
+        value:0,
+        min:0,
+        max:100,
+        unit:'%'
+    },
+    Opacity:{
+        value:100,
+        min:0,
+        max:100,
+        unit:'%'
+    },
+    Invert:{
+        value:0,
+        min:0,
+        max:100,
+        unit:'%'
+    },
+}
+
+applyFilters()
+  filtersContainer.innerHTML="";
+  createFilter();  
+})
+
+downloadBtn.addEventListener('click',()=>{
+    const link= document.createElement('a');
+    link.download= 'edited-image.png';
+    link.href= canvasImg.toDataURL();
+    link.click();
 })
